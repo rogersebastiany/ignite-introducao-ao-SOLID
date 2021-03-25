f3dc349e42bfdab180818f2425645405e1a808dc
Roger Sebastiany <rogersebastiany@gmail.com>
2021-03-25 16:23:24 -0300

    Merge branch 'main' of https://github.com/rogersebastiany/introducao-ao-SOLID into main

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -8,9 +8,13 @@ interface IRequest {
 
 class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
-
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
+  execute({ name, email }: IRequest): User {
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+    if (userAlreadyExists) {
+      throw new Error("E-mail Already Exists");
+    }
+    const user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
